ad18ba6c62104721179f1bee2ead367ed2cb3dd2
Josema <enzo@josema.es>
2019-10-13 11:18:25 +0200

    Added maxY and minY to createLimitPan

--- src/index.js
+++ src/index.js
@@ -29,14 +29,18 @@ export function changeRotation({
 // https://discourse.threejs.org/t/how-to-limit-pan-in-orbitcontrols-for-orthographiccamera/9061/7
 export function createLimitPan({ camera, controls, THREE }) {
     const v = new THREE.Vector3()
+    const minPan = new THREE.Vector3(minX, -Infinity, minZ)
+    const maxPan = new THREE.Vector3(maxX, Infinity, maxZ)
     return ({
         maxX = Infinity,
         minX = -Infinity,
+        maxY = Infinity,
+        minY = -Infinity,
         maxZ = Infinity,
         minZ = -Infinity
     }) => {
-        const minPan = new THREE.Vector3(minX, -Infinity, minZ)
-        const maxPan = new THREE.Vector3(maxX, Infinity, maxZ)
+        minPan.set(minX, minY, minZ)
+        maxPan.set(maxX, maxY, maxZ)
         v.copy(controls.target)
         controls.target.clamp(minPan, maxPan)
         v.sub(controls.target)
