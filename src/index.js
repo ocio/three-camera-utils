04d4d214a2910b57246a0eaa8ed4c0aee8feee6c
Josema <enzo@josema.es>
2020-03-23 11:45:32 +0100

    added screenToWorld and worldToScreen functions

--- src/index.js
+++ src/index.js
@@ -97,3 +97,47 @@ export function polarToCartesian(angleV, angleH, radius) {
         z: radius * Math.sin(phi) * Math.cos(theta)
     }
 }
+
+// https://stackoverflow.com/questions/27409074/converting-3d-position-to-2d-screen-position-r69
+export function worldToScreen({
+    x,
+    y,
+    z,
+    camera,
+    canvasWidth,
+    canvasHeight,
+    THREE
+}) {
+    const vector = new THREE.Vector3(x, y, z)
+    const widthHalf = canvasWidth / 2
+    const heightHalf = canvasHeight / 2
+
+    vector.project(camera)
+    const newX = vector.x * widthHalf + widthHalf
+    const newY = -(vector.y * heightHalf) + heightHalf
+
+    return { x: newX, y: newY }
+}
+
+// https://stackoverflow.com/questions/34660063/threejs-converting-from-screen-2d-coordinate-to-world-3d-coordinate-on-the-cane
+export function screenToWorld({
+    x,
+    y,
+    camera,
+    canvasWidth,
+    canvasHeight,
+    objects,
+    THREE
+}) {
+    const mouse = new THREE.Vector3(
+        (x / canvasWidth) * 2 - 1, //x
+        -(y / canvasHeight) * 2 + 1, //y
+        0.5
+    )
+
+    const raycaster = new THREE.Raycaster()
+    raycaster.setFromCamera(mouse, camera)
+    const intersects = raycaster.intersectObjects(objects)
+
+    return intersects
+}
